ccd85728bccef35443dde3be3bce7e5d10457d1e
Matthew Knorr <matt@knorrs.net>
2024-02-11 20:11:19 -0700

    Final Java program

--- Java/CurrencyConverter/Readme.md
+++ Java/CurrencyConverter/Readme.md
@@ -30,9 +30,7 @@ In addition to its technical prowess, the Currency Converter embodies a spirit o
 
 ## Software Demo Video
 
-{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running and a walkthrough of the code. Focus should be on sharing what you learned about the language syntax.}
-
-[Software Demo Video](http://youtube.link.goes.here)
+[Software Demo Video](https://youtu.be/QMkTyUyFI3Q)
 
 ## Development Environment
 
--- Java/CurrencyConverter/src/main/java/com/example/currencyconverter/Main.java
+++ Java/CurrencyConverter/src/main/java/com/example/currencyconverter/Main.java
@@ -1,157 +1,177 @@
-    package com.example.currencyconverter;
+package com.example.currencyconverter;
 
-    import javafx.application.Application;
-    import javafx.beans.property.SimpleStringProperty;
-    import javafx.geometry.Insets;
-    import javafx.scene.Scene;
-    import javafx.scene.control.*;
-    import javafx.scene.control.cell.PropertyValueFactory;
-    import javafx.scene.layout.*;
-    import javafx.scene.paint.Color;
-    import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-    import java.util.Arrays;
-    import java.util.List;
+import java.util.Arrays;
+import java.util.List;
 
-    public class Main extends Application {
+// Entry Point
+public class Main extends Application {
+    private static double exchangeRate = 0.000;
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Currency Converter");
 
-        private static double exchangeRate = 0.000;
+        Label amountLabel = new Label("Enter Amount:");
+        TextField amountField = new TextField();
+        Label exchangeRateLabel = new Label("Exchange Rate:");
+        Label rateValueLabel = new Label(String.valueOf(exchangeRate));
 
-        @Override
-        public void start(Stage primaryStage) {
-            primaryStage.setTitle("Currency Converter");
+        ComboBox<String> currencyComboBox = new ComboBox<>();
+        List<String> currencies = Arrays.asList(
+                "Select a currency", "$USD - United States Dollar", "€EUR - Euro", "$AUD - Australian Dollar", "£GBP - British Pound Sterling", "¥JPY - Japanese Yen", "$CAD - Canadian Dollar", "$NZD - New Zealand Dollar", "₪ILS - Israeli Shekel", "$HKD - Hong Kong Dollar", "krNOK - Norwegian Krone"
+        );
+        currencyComboBox.getItems().addAll(currencies);
+        currencyComboBox.setValue("Select a currency");
 
-            Label amountLabel = new Label("Enter Amount:");
-            TextField amountField = new TextField();
-            Label exchangeRateLabel = new Label("Exchange Rate:");
-            Label rateValueLabel = new Label(String.valueOf(exchangeRate));
+        TableView<Conversion> tableView = new TableView<>();
+        TableColumn<Conversion, String> amountColumn = new TableColumn<>("Amount");
+        TableColumn<Conversion, String> convertedColumn = new TableColumn<>("Converted");
+        tableView.getColumns().addAll(amountColumn, convertedColumn);
 
-            ComboBox<String> currencyComboBox = new ComboBox<>();
-            List<String> currencies = Arrays.asList(
-                    "Select a currency", "$USD - United States Dollar", "€EUR - Euro", "$AUD - Australian Dollar", "£GBP - British Pound Sterling", "¥JPY - Japanese Yen", "$CAD - Canadian Dollar", "$NZD - New Zealand Dollar", "₪ILS - Israeli Shekel", "$HKD - Hong Kong Dollar", "krNOK - Norwegian Krone"
-            );
-            currencyComboBox.getItems().addAll(currencies);
-            currencyComboBox.setValue("Select a currency");
+        amountColumn.setCellValueFactory(new PropertyValueFactory<>("amount"));
+        convertedColumn.setCellValueFactory(new PropertyValueFactory<>("convertedAmount"));
 
-            TableView<Conversion> tableView = new TableView<>();
-            TableColumn<Conversion, String> amountColumn = new TableColumn<>("Amount");
-            TableColumn<Conversion, String> convertedColumn = new TableColumn<>("Converted");
-            tableView.getColumns().addAll(amountColumn, convertedColumn);
 
-            amountColumn.setCellValueFactory(new PropertyValueFactory<>("amount"));
-            convertedColumn.setCellValueFactory(new PropertyValueFactory<>("convertedAmount"));
+        Label conversionLabel = new Label();
 
-            currencyComboBox.setOnAction(e -> {
-                String selectedCurrency = currencyComboBox.getValue();
-                exchangeRate = getExchangeRate(selectedCurrency);
-                rateValueLabel.setText(String.valueOf(exchangeRate));
-                tableView.getItems().clear();
-                populateTable(tableView, amountField.getText(), selectedCurrency);
-            });
+        currencyComboBox.setOnAction(e -> {
+            String selectedCurrency = currencyComboBox.getValue();
+            exchangeRate = getExchangeRate(selectedCurrency);
+            rateValueLabel.setText(String.valueOf(exchangeRate));
+            tableView.getItems().clear();
+            populateTable(tableView, amountField.getText(), selectedCurrency);
+            updateConversionLabel(amountField.getText(), selectedCurrency, conversionLabel);
+        });
 
-            amountField.setTextFormatter(new TextFormatter<>(change ->
-                    (change.getControlNewText().matches("\\d*\\.?\\d*")) ? change : null));
+        amountField.setTextFormatter(new TextFormatter<>(change ->
+                (change.getControlNewText().matches("\\d*\\.?\\d*")) ? change : null));
 
-            amountField.textProperty().addListener((observable, oldValue, newValue) -> {
-                tableView.getItems().clear();
-                populateTable(tableView, newValue, currencyComboBox.getValue());
-            });
+        amountField.textProperty().addListener((observable, oldValue, newValue) -> {
+            tableView.getItems().clear();
+            populateTable(tableView, newValue, currencyComboBox.getValue());
+            updateConversionLabel(newValue, currencyComboBox.getValue(), conversionLabel);
+        });
 
-            GridPane gridPane = new GridPane();
-            gridPane.setPadding(new Insets(20));
-            gridPane.setHgap(10);
-            gridPane.setVgap(10);
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(20));
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
 
-            BackgroundFill backgroundFill = new BackgroundFill(Color.rgb(30, 30, 30), CornerRadii.EMPTY, Insets.EMPTY);
-            Background background = new Background(backgroundFill);
-            gridPane.setBackground(background);
+        BackgroundFill backgroundFill = new BackgroundFill(Color.rgb(30, 30, 30), CornerRadii.EMPTY, Insets.EMPTY);
+        Background background = new Background(backgroundFill);
+        gridPane.setBackground(background);
 
-            amountLabel.setTextFill(Color.WHITE);
-            exchangeRateLabel.setTextFill(Color.WHITE);
-            rateValueLabel.setTextFill(Color.WHITE);
-            currencyComboBox.setStyle("-fx-background-color: white; -fx-text-fill: black;");
-            tableView.setStyle("-fx-background-color: #2f2f2f; -fx-text-fill: white;");
-            amountField.setStyle("-fx-background-color: #2f2f2f; -fx-text-fill: white;");
+        amountLabel.setTextFill(Color.WHITE);
+        exchangeRateLabel.setTextFill(Color.WHITE);
+        rateValueLabel.setTextFill(Color.WHITE);
+        currencyComboBox.setStyle("-fx-background-color: white; -fx-text-fill: black;");
+        tableView.setStyle("-fx-background-color: #2f2f2f; -fx-text-fill: white;");
+        amountField.setStyle("-fx-background-color: #2f2f2f; -fx-text-fill: white;");
+        conversionLabel.setStyle("-fx-text-fill: white");
 
-            gridPane.add(amountLabel, 0, 0);
-            gridPane.add(amountField, 1, 0);
-            gridPane.add(exchangeRateLabel, 0, 1);
-            gridPane.add(rateValueLabel, 1, 1);
-            gridPane.add(currencyComboBox, 0, 2, 2, 1);
-            gridPane.add(tableView, 0, 3, 2, 1);
+        gridPane.add(amountLabel, 0, 0);
+        gridPane.add(amountField, 1, 0);
+        gridPane.add(exchangeRateLabel, 0, 1);
+        gridPane.add(rateValueLabel, 1, 1);
+        gridPane.add(conversionLabel, 0, 3, 2, 1);
+        gridPane.add(currencyComboBox, 0, 4, 2, 1);
+        gridPane.add(tableView, 0, 5, 2, 1);
 
-            Scene scene = new Scene(gridPane, 400, 300);
-            primaryStage.setScene(scene);
+        Scene scene = new Scene(gridPane, 400, 300);
+        primaryStage.setScene(scene);
 
-            primaryStage.show();
-        }
+        primaryStage.show();
+    }
 
-        private double getExchangeRate(String currency) {
-            switch (currency.split(" - ")[0]) {
-                case "$USD":
-                    return 1;
-                case "€EUR":
-                    return 0.846540;
-                case "$AUD":
-                    return 1.354725;
-                case "£GBP":
-                    return 0.733893;
-                case "¥JPY":
-                    return 114.618000;
-                case "$CAD":
-                    return 1.270743;
-                case "$NZD":
-                    return 1.469310;
-                case "₪ILS":
-                    return 3.239470;
-                case "$HKD":
-                    return 7.765971;
-                case "krNOK":
-                    return 8.810382;
-                default:
-                    return 0.000;
-            }
-        }
-
-        private void populateTable(TableView<Conversion> tableView, String amountString, String selectedCurrency) {
-            try {
-                double amount = Double.parseDouble(amountString);
-                double convertedAmount;
-                int nextStep = (int) Math.ceil(amount);
-                for (int i = 1; i <= nextStep; i *= 10) {
-                    if (i > amount) {
-                        nextStep = i;
-                        break;
-                    }
-                }
-                for (int i = 1; i <= nextStep; i *= 10) {
-                    convertedAmount = i * exchangeRate;
-                    tableView.getItems().add(new Conversion(String.valueOf(i), String.format("%.2f", convertedAmount)));
-                }
-            } catch (NumberFormatException ex) {
-                // Handle exception
-            }
-        }
-
-        public static void main(String[] args) {
-            launch(args);
-        }
-
-        public static class Conversion {
-            private final SimpleStringProperty amount;
-            private final SimpleStringProperty convertedAmount;
-
-            public Conversion(String amount, String convertedAmount) {
-                this.amount = new SimpleStringProperty(amount);
-                this.convertedAmount = new SimpleStringProperty(convertedAmount);
-            }
-
-            public String getAmount() {
-                return amount.get();
-            }
-
-            public String getConvertedAmount() {
-                return convertedAmount.get();
-            }
+    // Currency Selection
+    private double getExchangeRate(String currency) {
+        switch (currency.split(" - ")[0]) {
+            case "$USD":
+                return 1;
+            case "€EUR":
+                return 0.846540;
+            case "$AUD":
+                return 1.354725;
+            case "£GBP":
+                return 0.733893;
+            case "¥JPY":
+                return 114.618000;
+            case "$CAD":
+                return 1.270743;
+            case "$NZD":
+                return 1.469310;
+            case "₪ILS":
+                return 3.239470;
+            case "$HKD":
+                return 7.765971;
+            case "krNOK":
+                return 8.810382;
+            default:
+                return 0.000;
         }
     }
+
+    // Populate/Refresh Table
+    private void populateTable(TableView<Conversion> tableView, String amountString, String selectedCurrency) {
+        try {
+            double amount = Double.parseDouble(amountString);
+            double convertedAmount;
+            int nextStep = (int) Math.ceil(amount);
+            for (int i = 1; i <= nextStep; i *= 10) {
+                if (i > amount) {
+                    nextStep = i;
+                    break;
+                }
+            }
+            for (int i = 1; i <= nextStep; i *= 10) {
+                convertedAmount = i * exchangeRate;
+                tableView.getItems().add(new Conversion(String.valueOf(i), String.format("%.2f", convertedAmount)));
+            }
+        } catch (NumberFormatException ex) {
+            // Handle exception
+        }
+    }
+
+    // Method to update the conversion label
+    private void updateConversionLabel(String amountString, String selectedCurrency, Label conversionLabel) {
+        try {
+            double amount = Double.parseDouble(amountString);
+            double convertedAmount = amount * exchangeRate;
+            conversionLabel.setText("Converted Amount: " + String.format("%.2f", convertedAmount) + " " + selectedCurrency.split(" - ")[0].substring(1));
+        } catch (NumberFormatException ex) {
+            // Handle exception
+            conversionLabel.setText("");
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public static class Conversion {
+        private final SimpleStringProperty amount;
+        private final SimpleStringProperty convertedAmount;
+
+        public Conversion(String amount, String convertedAmount) {
+            this.amount = new SimpleStringProperty(amount);
+            this.convertedAmount = new SimpleStringProperty(convertedAmount);
+        }
+
+        public String getAmount() {
+            return amount.get();
+        }
+
+        public String getConvertedAmount() {
+            return convertedAmount.get();
+        }
+    }
+}
